35a87f8185f4dbcdfdeb6e8353da0e929b13f888
Norberts <mainanorbert@gmail.comm>
2024-02-21 11:57:48 +0300

    added documentation for contribution views

--- wms/contributions/views.py
+++ wms/contributions/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -13,17 +12,43 @@ from app1.authenticate import AllowUnauthenticated
 from rest_framework import permissions
 
 class ContributionsViews(viewsets.ModelViewSet):
+    """
+    A viewset for handling CRUD operations on Contributions.
+
+    Attributes:
+        queryset (QuerySet): The queryset of Contributions.
+        serializer_class (Serializer): The serializer class for Contributions.
+        permission_classes (list): The permission classes for Contributions views.
+    """
     queryset = Contributions.objects.all()
     serializer_class = ContributionSerializers
     permission_classes = [AllowUnauthenticated | permissions.IsAuthenticated]
 
 class AllContributions(viewsets.ModelViewSet):
+    """
+    A viewset for handling CRUD operations on AllContributions.
+
+    Attributes:
+        queryset (QuerySet): The queryset of AllContributions.
+        serializer_class (Serialiszer): The serializer class for AllContributions.
+        permission_classes (list): The permission classes for AllContributions views.
+    """
     queryset = AllContribution.objects.all()
     serializer_class = AllContributionsSerializers
     permission_classes = [AllowUnauthenticated | permissions.IsAuthenticated]
     
     @action(detail=True, methods=['get'])
     def contribution_list(self, req, pk=None):
+        """
+        Custom action to get a list of contributions for a specific cont_id.
+
+        Args:
+            req: The request object.
+            pk (int): The cont_id to filter contributions.
+
+        Returns:
+            Response: A response containing the list of contributions for the cont_id.
+        """
         total_amount = 0
         
         contributions = AllContribution.objects.filter(cont_id=pk)
@@ -45,5 +70,3 @@ class AllContributions(viewsets.ModelViewSet):
             })
         cont_data.append({"total_amount": total_amount})
         return Response(cont_data)
-        
-        
